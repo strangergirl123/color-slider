adfd7248861e0c9f5bd7dfe769ac5f28bcc5f145
Jessica Lu <jl10698@nyu.edu>
2020-03-03 09:58:35 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 let slider1;
+let slider2;
+let slider3;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     slider1=createSlider(0, 255);
+    slider2=createSlider(0,255);
+    slider3=createSlider(0,255);
 }
 
 function draw(){
-    background(slider1.value());
+    background(slider1.value(),slider2.value(),slider3.value());
 }
